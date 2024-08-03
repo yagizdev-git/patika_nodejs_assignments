7c9edb01f856cfe050a60c41c963c74784b414de
Yagiz <146383293+yagizdev-git@users.noreply.github.com>
2024-08-03 16:18:35 +0300

    documents have been setted

--- 've done/cicrcleArea.js
+++
@@ -1,9 +0,0 @@
-const radius = parseInt((process.argv.slice(2))[0]);
-const pi = 3.141592;
-
-function calculateArea (r) {
-  let area = (pi)*(r**2);
-  console.log(`Yarıçapı "${r}" olan dairenin alanı: "${area}"`);
-}
-
-calculateArea(radius);
