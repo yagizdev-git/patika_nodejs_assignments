66a9e87fef85753f15d2ae1c42b715bf25201d29
Yagiz <146383293+yagizdev-git@users.noreply.github.com>
2024-08-04 07:39:18 +0300

    task3 module concept committed

---
+++ task3-module/circle.js
@@ -0,0 +1,12 @@
+function circleArea (radius) {
+  return Math.PI * radius * radius;
+};
+
+function circleCircumference (radius) {
+  return 2 * Math.PI * radius ;
+};
+
+module.exports = {
+  circleArea,
+  circleCircumference
+};
---
+++ task3-module/index.js
@@ -0,0 +1,5 @@
+const { circleArea, circleCircumference } = require("./circle");
+
+const myRadius = 5;
+console.log(`Dairenin alanı: ${circleArea(myRadius)}`);
+console.log(`Dairenin çevresi: ${circleCircumference(myRadius)}`);
